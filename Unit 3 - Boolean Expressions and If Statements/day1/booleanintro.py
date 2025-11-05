77e94bb6b0e2348415ca8410158f02e52a5abf36
James Kraft <jkraft@bayviewglen.ca>
2025-11-05 15:17:24 -0500

    boolean expressions

--- Unit 3 - Boolean Expressions and If Statements/day1/booleanintro.py
+++ Unit 3 - Boolean Expressions and If Statements/day1/booleanintro.py
@@ -13,20 +13,22 @@ target = 20.0
 print(length != target)
 
 username = "Admin"
-print(username.lower() == "admin")
+print(username.lower() == "admin") # true
 
 msg = "System OK"
-print("ok" in msg.lower())
+print("ok" in msg.lower()) # True, ok is in "System OK".lower()
 
 password = "secret42"
-print(len(password) >= 8)
+print(len(password) >= 8) # True
 
 word = "preheating"
 print(word[:3].lower() == "pre")  # starts with
 print(word[-3:].lower() == "ing") # ends with
 
 title = "The Hobbit"
-print(title[:3].lower() == "the")
+print(title[:3].lower() == "the") # True
 
 name = "maria"
 print(name != "" and name[0].lower() < "m")  # alphabet check (guard empty)
+# Making sure that name is not an empty string. If name was an empty string, it would crash. 
+# And means that both of them have to be true. 
